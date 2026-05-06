80b9aa7efe133269de77dc765ebd06896eaba2fb
Ivannuell <ivannguminang0@gmail.com>
2026-05-06 17:04:54 +0800

    Refactoring initializing indices of spaceships

--- Scenes/Game/Systems/TypingAttackSystem.gd
+++ Scenes/Game/Systems/TypingAttackSystem.gd
@@ -1,7 +1,7 @@
 extends Node
 
 var enemy_inRange: Array = []
-var active_Enemy
+var active_Enemy: Spaceship
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,13 +18,17 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventKey and event.pressed and not event.echo:
 		var input = char(event.unicode)
 		
+		if event.unicode == 0:
+			return
+		
 		if input.is_valid_int():
 			var target_index = int(input)
-			if target_index >= 1 or target_index <= 9:
+			if target_index >= 1 and target_index <= 9:
 				for i in enemy_inRange:
 					if i.index == target_index:
 						SignalBus.emit_signal("activate_enemy", i)
 						active_Enemy = i
+						break
 					
 		if not active_Enemy:
 			return
@@ -32,15 +36,19 @@ func _input(event: InputEvent) -> void:
 		if active_Enemy.check_input(input):
 			if active_Enemy.is_word_complete():
 				active_Enemy.on_word_completed()
+				active_Enemy = null
+				call_deferred("inititalize_enemy_list")
 
 func inititalize_enemy_list():
 	var index = 1
 	for i in enemy_inRange:
 		i.update_index(index)
 		index += 1
-		
+
 	if active_Enemy == null and enemy_inRange.size() > 0:
 		SignalBus.emit_signal("activate_enemy", enemy_inRange[0])
+		
+	
 
 func on_enemy_enter(enemy: Spaceship):
 	enemy_inRange.append(enemy)
@@ -53,8 +61,9 @@ func on_enemy_exit(enemy: Spaceship):
 	if enemy_inRange.is_empty():
 		SignalBus.emit_signal("no_enemy")
 		active_Enemy = null
-
-	inititalize_enemy_list()
+	else:
+		active_Enemy = null
+		inititalize_enemy_list()
 
 func on_activate_enemy(enemy: Spaceship):
 	for i in enemy_inRange:
